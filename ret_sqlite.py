663caf8032cc819b742a9c38bc537ce00a8cfb66
Nicholas Vu <nicholas.t.vu@gmail.com>
2015-06-18 18:58:47 -0700

    create ret_sqlite.py

---
+++ ret_sqlite.py
@@ -0,0 +1,19 @@
+import sqlite3 as lite
+import pandas as pd
+
+con = lite.connect('getting_started.db')
+
+with con:
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM cities")
+
+    rows = cur.fetchall()
+    cols = [desc[0] for desc in cur.description]
+    df = pd.DataFrame(rows)
+
+    for col in cols:
+        print col
+        
+    for row in rows:
+        print row
