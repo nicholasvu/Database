a0a0c17ea7eb21ee29dd8d19e5b9f002807f66fc
Nicholas Vu <nicholas.t.vu@gmail.com>
2015-06-23 11:05:28 -0500

    Create QQ_Plot_Norm.py

---
+++ QQ_Plot_Norm.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+plt.figure()
+test_data = np.random.normal(size=1000)   
+graph1 = stats.probplot(test_data, dist="norm", plot=plt)
+plt.show() #this will generate the first graph
